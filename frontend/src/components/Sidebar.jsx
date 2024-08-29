ae1eadb401b7836580d5bccaadf256eb35f4d21e
DaVinci <siddharthkotak280@gmail.com>
2024-08-29 19:03:14 +0530

    made sidebar functional and made it responsive

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,105 +4,195 @@ import "./Sidebar.css";
 import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
-	// const [isOpen, setIsOpen] = useState(false);
-	const { isOpen, setIsOpen, user } = useContext(AppContext);
+  // const [isOpen, setIsOpen] = useState(false);
+  const [isPopupOpen, setPopupOpen] = useState(false);
 
-	const navigate = useNavigate();
+  const togglePopup = () => {
+    setPopupOpen(!isPopupOpen);
+  };
 
-	const toggleSidebar = () => {
-		setIsOpen(!isOpen);
-	};
-	useEffect(() => {
-		const token = localStorage.getItem("token");
-		// if (!token) navigate("/signin");
-		if (token) {
-			const payload = JSON.parse(atob(token.split(".")[1]));
-			const expiration = new Date(payload.exp * 1000);
-			const now = new Date();
+  const { isOpen, setIsOpen, user } = useContext(AppContext);
 
-			if (now >= expiration) {
-				localStorage.removeItem("token");
-				localStorage.setItem("loggedIn", "false");
-				navigate("/signin");
-				toast.error("Session expired, please log in again.");
-			} else {
-				// Set a timeout to remove the token just before it expires
-				const timeout = expiration.getTime() - now.getTime();
-				setTimeout(() => {
-					localStorage.removeItem("token");
-					localStorage.setItem("loggedIn", "false");
-					navigate("/signin");
-					toast.error("Session expired, please log in again.");
-				}, timeout);
-			}
-		}
-	}, []);
+  const navigate = useNavigate();
 
-	return (
-		<div className={`sidebar ${isOpen ? "open" : ""}`}>
-			<div className="logo-details flex items-center">
-				<i className="bx bxs-school icon ml-2"></i>
-				<div className="logo_name flex items-center mt-1">UniAsk</div>
-				<i className="bx bx-menu" id="btn" onClick={toggleSidebar}></i>
-			</div>
-			<ul className="nav-list">
-				<li>
-					<button>
-						<i className="bx bx-grid-alt"></i>
-						<span className="links_name">Dashboard</span>
-					</button>
-					<span className="tooltip">Dashboard</span>
-				</li>
-				<li>
-					<button>
-						<i className="bx bx-user"></i>
-						<span className="links_name">Profile</span>
-					</button>
-					<span className="tooltip">Profile</span>
-				</li>
-				<li>
-					<button>
-						<i className="bx bx-chat"></i>
-						<span className="links_name">Chats</span>
-					</button>
-					<span className="tooltip">Chats</span>
-				</li>
-				<li>
-					<button>
-						<i className="bx bx-heart"></i>
-						<span className="links_name">Saved</span>
-					</button>
-					<span className="tooltip">Saved</span>
-				</li>
-				<li>
-					<button>
-						<i className="bx bx-cog"></i>
-						<span className="links_name">Setting</span>
-					</button>
-					<span className="tooltip">Setting</span>
-				</li>
-				<li className="profile">
-					<div className="profile-details">
-						<div className="flex items-center mb-4">
-							<div className="h-11 w-11 rounded-md mr-2 flex items-center justify-center bg-black text-white text-xl font-bold shadow-glow">
-								{user.name.charAt(0)}
-							</div>
-						</div>
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    // if (!token) navigate("/signin");
+    if (token) {
+      const payload = JSON.parse(atob(token.split(".")[1]));
+      const expiration = new Date(payload.exp * 1000);
+      const now = new Date();
 
-						<div className="name_job">
-							<div className="name">{user.name}</div>
-							<div className="job">
-								{user.branch} {user.batch} Student
-							</div>
-						</div>
-					</div>
-					<button>
-						<i className="bx bx-log-out" id="log_out"></i>
-					</button>
-				</li>
-			</ul>
-		</div>
-	);
+      if (now >= expiration) {
+        localStorage.removeItem("token");
+        localStorage.setItem("loggedIn", "false");
+        navigate("/signin");
+        toast.error("Session expired, please log in again.");
+      } else {
+        // Set a timeout to remove the token just before it expires
+        const timeout = expiration.getTime() - now.getTime();
+        setTimeout(() => {
+          localStorage.removeItem("token");
+          localStorage.setItem("loggedIn", "false");
+          navigate("/signin");
+          toast.error("Session expired, please log in again.");
+        }, timeout);
+      }
+    }
+  }, []);
+
+  return (
+    <div className={`sidebar ${isOpen ? "open" : ""}`}>
+      <div className="logo-details flex items-center">
+        <i className="bx bxs-school icon ml-2"></i>
+        <div className="logo_name flex items-center mt-1">UniAsk</div>
+        <i className="bx bx-menu" id="btn" onClick={toggleSidebar}></i>
+      </div>
+      <ul className="nav-list">
+        <li>
+          <button
+            onClick={() => {
+              navigate(`/dashboard/${user.id}`);
+              setIsOpen(false);
+            }}
+          >
+            <i className="bx bx-grid-alt"></i>
+            <span className="links_name">Dashboard</span>
+          </button>
+          <span className="tooltip">Dashboard</span>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              navigate("/reguser");
+              setIsOpen(false);
+            }}
+          >
+            <i className="bx bx-user"></i>
+            <span className="links_name">Users</span>
+          </button>
+          <span className="tooltip">Users</span>
+        </li>
+        <li className="relative">
+          <button onClick={togglePopup}>
+            <i className="bx bx-chat"></i>
+            <span className="links_name">ChatRoom</span>
+          </button>
+          <span className="tooltip">ChatRoom</span>
+
+          {isPopupOpen && (
+            <div className="popup fixed inset-0 flex items-center justify-center z-50">
+              <div className="popup-content bg-gray-800 text-white p-5 rounded-lg shadow-lg">
+                <h3 className="text-lg font-semibold mb-4">Select Option</h3>
+                <div className="flex space-x-4">
+                  <button
+                    onClick={() => {
+                      navigate("/chat/all");
+                      setIsOpen(false);
+                      togglePopup();
+                    }}
+                    className="bg-gray-700 hover:bg-gray-600 py-2 px-4 rounded"
+                  >
+                    All
+                  </button>
+                  <button
+                    onClick={() => {
+                      navigate(`/chat/${user.branch}`);
+                      setIsOpen(false);
+                      togglePopup();
+                    }}
+                    className="bg-gray-700 hover:bg-gray-600 py-2 px-4 rounded"
+                  >
+                    {user.branch}
+                  </button>
+                  <button
+                    onClick={() => {
+                      navigate(`/chat/${user.batch}`);
+                      setIsOpen(false);
+                      togglePopup();
+                    }}
+                    className="bg-gray-700 hover:bg-gray-600 py-2 px-4 rounded"
+                  >
+                    {user.batch}
+                  </button>
+                </div>
+                <button
+                  onClick={togglePopup}
+                  className="mt-4 bg-red-600 hover:bg-red-500 py-2 px-4 rounded"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          )}
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              navigate("/doubts");
+              setIsOpen(false);
+            }}
+          >
+            <i className="bx bx-file"></i>
+            <span className="links_name">Doubts</span>
+          </button>
+          <span className="tooltip">Doubts</span>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              navigate("/postDoubt");
+              setIsOpen(false);
+            }}
+          >
+            <i className="bx bx-comment-dots"></i>
+            <span className="links_name">PostDoubt</span>
+          </button>
+          <span className="tooltip">PostDoubt</span>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              navigate("/");
+              setIsOpen(false);
+            }}
+          >
+            <i className="bx bx-heart"></i>
+            <span className="links_name">Saved</span>
+          </button>
+          <span className="tooltip">Saved</span>
+        </li>
+        <li className="profile w-full">
+          <div className="profile-details">
+            <div className="flex items-center mb-4">
+              <div className="h-11 w-11 rounded-md mr-2 flex items-center justify-center bg-black text-white text-xl font-bold shadow-glow">
+                {user.name.charAt(0)}
+              </div>
+            </div>
+
+            <div className="name_job flex-1">
+              <div className="name truncate">{user.name}</div>
+              <div className="job">
+                {user.branch} {user.batch} Student
+              </div>
+            </div>
+          </div>
+          <button
+            onClick={() => {
+              localStorage.clear();
+              navigate("/signin");
+            }}
+            className="logout-btn"
+          >
+            <i className="bx bx-log-out" id="log_out"></i>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
